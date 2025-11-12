4ae554c9013772d976605cdf21b440945ac450ee
rschleitzer <ralf@scaly.io>
2025-11-12 15:02:06 +0100

    Initialize TypeScript monorepo with core interfaces from OpenJade

---
+++ packages/dazzle-core/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Dazzle Core - TypeScript port of OpenJade's DSSSL processor
+ *
+ * This is the core library containing:
+ * - Grove interfaces (abstract document tree)
+ * - FOT Builder interfaces (backend abstraction)
+ * - Scheme interpreter (bytecode VM)
+ * - DSSSL processing model
+ * - Primitives (260 language features)
+ */
+
+export * from './grove';
+export * from './fot';
